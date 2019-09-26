386b68301897ecdc8c962692c172edc43f234803
Alexis Taberna <alexiscaspell@gmail.com>
2019-09-26 00:00:08 -0300

    Retoco graficos

--- simulacion.py
+++ simulacion.py
@@ -2,7 +2,7 @@ import configuracion
 import tareas
 from tareas import TipoTarea
 from abc import ABCMeta
-from typing import List,Tuple
+from typing import List, Tuple
 from administradores import Administrador
 from tareas import Tarea
 from configuracion import UnidadTiempo
@@ -11,67 +11,77 @@ import numpy as np
 
 
 class Metrica(ABCMeta):
-    def __init__(self,metrica_spec):
+    def __init__(self, metrica_spec):
         self.nombre = metrica_spec["nombre"]
 
-        grafico = metrica_spec.get("grafico",{})
-        self.formato_grafico = grafico.get("extension","svg")
-        self.guardar_grafico = grafico.get("activo",False)
-        self.path_grafico = grafico.get("nombre_archivo",self.nombre)
+        grafico = metrica_spec.get("grafico", {})
+        self.formato_grafico = grafico.get("extension", "svg")
+        self.guardar_grafico = grafico.get("activo", False)
+        self.path_grafico = grafico.get("nombre_archivo", self.nombre)
 
     def calcular(self, simulacion):
         raise NotImplementedError(
             "Metodo a implementar por metrica no cumplido")
-            
+
     def guardar_grafico(self):
         if self.guardar_grafico:
-            plt.savefig(f"{self.path_grafico}.{self.formato_grafico}", format=self.formato_grafico)
+            plt.savefig(f"{self.path_grafico}.{self.formato_grafico}",
+                        format=self.formato_grafico)
         plt.close()
+
 
 class FactoryMetricas:
 
     @staticmethod
     def crear(metric_spec: dict) -> Metrica:
-        if metric_spec["nombre"]=="tiempo_ocioso":
+        if metric_spec["nombre"] == "tiempo_ocioso":
             return TiempoOcioso(metric_spec)
-        elif metric_spec["nombre"]=="tiempo_resolucion_promedio":
+        elif metric_spec["nombre"] == "tiempo_resolucion_promedio":
             return TiempoDeResolucionPromedio(metric_spec)
-        elif metric_spec["nombre"]=="porcentaje_tareas_realizadas":
+        elif metric_spec["nombre"] == "porcentaje_tareas_realizadas":
             return PorcentajeDeTareasRealizadas(metric_spec)
-        
+
 ###########################################################--- SIMULACION ---###########################################################
+
+
 class Evento:
-    def __init__(self,tarea):
+    def __init__(self, tarea):
         self.tarea = tarea
-    @property    
+
+    @property
     def tiempo(self):
         raise NotImplementedError("Propiedad tiempo no encontrada")
 
+
 class EventoLlegada(Evento):
-    def __init__(self,tarea):
+    def __init__(self, tarea):
         if not tarea:
             raise ValueError("No se paso una tarea para el evento de llegada")
         self.tarea = tarea
 
-    @property    
+    @property
     def tiempo(self):
         return self.tarea.tiempo_creacion
 
+
 class EventoSalida(Evento):
-    def __init__(self,tarea):
+    def __init__(self, tarea):
         if not tarea:
             raise ValueError("No se paso una tarea para el evento de salida")
         self.tarea = tarea
 
-    @property    
+    @property
     def tiempo(self):
         return self.tarea.tiempo_fin
 
-def crear_eventos_llegada(tareas:List[Tarea])->List[EventoLlegada]:
-    return list(map(lambda t: EventoLlegada(t),tareas))
-    
-def crear_eventos_salida(tareas:List[Tarea])->List[EventoSalida]:
-    return list(map(lambda t: EventoSalida(t),tareas))
+
+def crear_eventos_llegada(tareas: List[Tarea]) -> List[EventoLlegada]:
+    return list(map(lambda t: EventoLlegada(t), tareas))
+
+
+def crear_eventos_salida(tareas: List[Tarea]) -> List[EventoSalida]:
+    return list(map(lambda t: EventoSalida(t), tareas))
+
 
 class Simulacion:
     def __init__(self, configuracion, lista_tareas, administradores):
@@ -86,7 +96,7 @@ class Simulacion:
         self.tiempo_fin = configuracion.tiempo_fin_simulacion
         self.administradores = administradores
 
-    def _asignar(self, lista_tareas: List[Tarea]) -> Tuple[List,List]:
+    def _asignar(self, lista_tareas: List[Tarea]) -> Tuple[List, List]:
         '''Asigna las tareas que pueden ser asignadas y retorna tupla tareas_asignadas,tareas_sin_asignar'''
         tareas_sin_asignar = []
         tareas_asignadas = []
@@ -100,38 +110,39 @@ class Simulacion:
                 if admin.alguien_puede_resolver(tarea):
                     posibles_resolutores.append(admin)
 
-            ordenamiento = lambda a : a.menor_tiempo_comprometido
+            def ordenamiento(a): return a.menor_tiempo_comprometido
             posibles_resolutores.sort(key=ordenamiento)
 
-            admin = posibles_resolutores[0] if tarea.perfil is None else next(a for a in self.administradores if a.perfil==tarea.perfil)
+            admin = posibles_resolutores[0] if tarea.perfil is None else next(
+                a for a in self.administradores if a.perfil == tarea.perfil)
 
-            tarea_actualizada = admin.resolver_tarea(self.tiempo_sistema,tarea)
-            
+            tarea_actualizada = admin.resolver_tarea(
+                self.tiempo_sistema, tarea)
+
             tareas_asignadas.append(tarea_actualizada)
-            
-        return tareas_asignadas,tareas_sin_asignar
 
-    def resolver(self,evento:Evento):
+        return tareas_asignadas, tareas_sin_asignar
+
+    def resolver(self, evento: Evento):
         '''Resuelve dependiendo del tipo de evento y retorna los nuevos eventos condicionales y no condicionales'''
-        llegadas=[]
-        salidas=[]
+        llegadas = []
+        salidas = []
 
         for a in self.administradores:
             a.actualizar_tiempo_ocioso(self.tiempo_sistema)
 
-        if(isinstance(evento,EventoLlegada)):
+        if(isinstance(evento, EventoLlegada)):
             print(f"LLEGADA DE {evento.tarea.tipo_tarea.value} ...")
-            asignadas,sin_asignar = self._asignar([evento.tarea])
+            asignadas, sin_asignar = self._asignar([evento.tarea])
             llegadas = crear_eventos_llegada(sin_asignar)
             salidas = crear_eventos_salida(asignadas)
 
-            #ACA SE GUARDAN LAS TAREAS QUE SE COMENZARON Y NUNCA SE TERMINARON
-            self.tareas_asignadas = self.tareas_asignadas+list(filter(lambda t:t.tiempo_fin>=self.tiempo_fin,asignadas))
+            # ACA SE GUARDAN LAS TAREAS QUE SE COMENZARON Y NUNCA SE TERMINARON
+            self.tareas_asignadas = self.tareas_asignadas + \
+                list(filter(lambda t: t.tiempo_fin >= self.tiempo_fin, asignadas))
 
-        elif isinstance(evento,EventoSalida):
+        elif isinstance(evento, EventoSalida):
             print(f"SALIDA DE {evento.tarea.perfil.value} ...")
-            # admin = next(a for a in self.administradores if a.perfil==evento.tarea.perfil)
-            # admin.finalizar_tarea(evento.tarea)
             self.tareas_finalizadas.append(evento.tarea)
 
         return llegadas+salidas
@@ -140,46 +151,67 @@ class Simulacion:
         return list(map(lambda m: {m.nombre: m.calcular(self)}, self.metricas))
 
 ###########################################################--- METRICAS ---###########################################################
+
+
 class TiempoOcioso:
 
-    def __init__(self,data):
-        Metrica.__init__(self,data)
+    def __init__(self, data):
+        Metrica.__init__(self, data)
 
     def calcular(self, simulacion: Simulacion):
         resultado = {}
         for a in simulacion.administradores:
             perfil = a.perfil
-            porcentaje_ocioso = a.tiempo_ocioso/(simulacion.tiempo_fin*a.programadores)
-            resultado.update({perfil.value:porcentaje_ocioso})
+            porcentaje_ocioso = a.tiempo_ocioso / \
+                (simulacion.tiempo_fin*a.programadores)
+            resultado.update({perfil.value: porcentaje_ocioso})
 
-        self.resultado=resultado
+        self.resultado = resultado
 
         return self.resultado
 
     def generar_grafico(self):
-        tiempos_ocio = [v*100 for v in self.resultado.values()]
-        x = np.arange(len(self.resultado))
-        plt.bar(x, tiempos_ocio)
-        plt.xticks(x, tuple([k for k in self.resultado]))
-        
+
+        tuplas = list(self.resultado.items())
+
+        tuplas.sort(key=lambda e: e[1])
+
+        porcentajes = [float(f"{r[1]*100:3.2f}") for r in tuplas]
+        perfiles = [t[0] for t in tuplas]
+
+        x_pos = [i for i, _ in enumerate(perfiles)]
+
+        plt.bar(x_pos, porcentajes, color='green')
+        plt.xlabel("Perfil")
+        plt.ylabel(f"Porcentaje (%)")
+        plt.title("PORCENTAJE DE TIEMPO OCIOSO")
+
+        plt.xticks(x_pos, perfiles)
+
+        plt.tight_layout()
+
         Metrica.guardar_grafico(self)
+
 
 class TiempoDeResolucionPromedio:
 
-    def __init__(self,data):
-        self.unidad_tiempo=UnidadTiempo(data.get("unidad_tiempo",UnidadTiempo.Segundos))
-        Metrica.__init__(self,data)
+    def __init__(self, data):
+        self.unidad_tiempo = UnidadTiempo(
+            data.get("unidad_tiempo", UnidadTiempo.Segundos))
+        Metrica.__init__(self, data)
 
     def calcular(self, simulacion: Simulacion):
         tiempos_promedio_por_tipo_tarea = {}
 
         for tipo_tarea in TipoTarea:
-            tareas_de_tipo_tarea:List[Tarea] = list(
+            tareas_de_tipo_tarea: List[Tarea] = list(
                 filter(lambda t: t.tipo_tarea == tipo_tarea, simulacion.tareas_finalizadas))
             cant_tareas = max(len(tareas_de_tipo_tarea), 1)
-                
-            promedio = sum(map(lambda t: t.tiempo_fin - t.tiempo_creacion, tareas_de_tipo_tarea))/cant_tareas
-            promedio = UnidadTiempo.Segundos.llevar_a(self.unidad_tiempo,promedio)
+
+            promedio = sum(map(lambda t: t.tiempo_fin -
+                               t.tiempo_creacion, tareas_de_tipo_tarea))/cant_tareas
+            promedio = UnidadTiempo.Segundos.llevar_a(
+                self.unidad_tiempo, promedio)
             tiempos_promedio_por_tipo_tarea.update(
                 {tipo_tarea.value: promedio})
 
@@ -188,31 +220,63 @@ class TiempoDeResolucionPromedio:
         return self.resultado
 
     def generar_grafico(self):
-        tiempos_promedio = self.resultado.values()
-        x = np.arange(len(self.resultado))
-        plt.bar(x, tiempos_promedio)
-        plt.xticks(x, tuple([k for k in self.resultado]))
+
+        tuplas = list(self.resultado.items())
+
+        tuplas.sort(key=lambda e: e[1])
+
+        tiempos_promedio = [float(f"{r[1]:9.2f}") for r in tuplas]
+        tipos_tarea = [t[0] for t in tuplas]
+
+        x_pos = [i for i, _ in enumerate(tipos_tarea)]
+
+        plt.bar(x_pos, tiempos_promedio, color='green')
+        plt.xlabel("Tarea")
+        plt.ylabel(f"Tiempo ({self.unidad_tiempo.value})")
+        plt.title("PROMEDIO DE RESOLUCION DE TAREAS")
+
+        plt.xticks(x_pos, tipos_tarea)
+
+        plt.tight_layout()
+
         Metrica.guardar_grafico(self)
 
+
 class PorcentajeDeTareasRealizadas:
-    
-    def __init__(self,data):
-        Metrica.__init__(self,data)
+
+    def __init__(self, data):
+        Metrica.__init__(self, data)
 
     def calcular(self, simulacion: Simulacion):
-        self.resultado =  len(simulacion.tareas_finalizadas)/len(simulacion.tareas)
+        self.resultado = len(simulacion.tareas_finalizadas) / \
+            len(simulacion.tareas)
 
         return self.resultado
 
     def generar_grafico(self):
-        labels = ['Hechas', 'Restantes']
-        sizes = [self.resultado*100,(1-self.resultado)*100]
-        colors = ['lightskyblue', 'gold']
-        patches, texts = plt.pie(sizes, colors=colors, shadow=True, startangle=90)
-        plt.legend(patches, labels, loc="best")
-        plt.axis('equal')
+
+        fig, ax = plt.subplots(subplot_kw=dict(aspect="equal"))
+
+        etiquetas = ['Hechas', 'Restantes']
+        porcentajes = [self.resultado, 1-self.resultado]
+
+        def func(pct, allvals):
+            return f"{pct:3.2f}%"
+
+        wedges, texts, autotexts = ax.pie(porcentajes, autopct=lambda pct: func(pct, porcentajes),
+                                          textprops=dict(color="w"))
+
+        ax.legend(wedges, etiquetas,
+                  title="Tareas",
+                  loc="center left",
+                  bbox_to_anchor=(1, 0, 0.5, 1))
+
+        plt.title("PORCENTAJE DE TAREAS REALIZADAS")
+
         plt.tight_layout()
+
         Metrica.guardar_grafico(self)
+
 
 if __name__ == "__main__":
     pass
